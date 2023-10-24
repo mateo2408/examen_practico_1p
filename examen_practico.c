2e4062894c92a7167288c92857938a424e7bd769
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-24 15:03:50 -0500

    Creacion de funcion principal y definir variables

--- examen_practico.c
+++ examen_practico.c
@@ -8,7 +8,7 @@ El programa debe detenerse cuando el usuario ingrese el número 0.*/
 // Función para calcular la suma de una serie de números
 int suma (int n){
     // Si n es 0, significa que se ha terminado de leer la serie
-    if (n ==0)
+    if (n == 0)
     {
         return 0;
         // Suma el número actual con la suma de los números anteriores
@@ -17,69 +17,17 @@ int suma (int n){
     }
 }
 
+//Funcion principal
 int main()
 {
-    int num;
+    //Declaramos una variable para almacenar la suma
+    int suma = 0;
 
-    while (num =! 0 )
-    {
-        printf("Ingrese un numero entero -> ");
-        scanf("%d", &num);
-    }
+    //Leemos el primer numero
+    int n;
+    printf("Ingrese un numero entero(Ingrese 0 para terminar) -> ");
+    scanf("%d", &n);
+
     
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*#include <stdio.h>
-
-// Función para calcular la suma de una serie de números
-int suma(int n) {
-  // Si n es 0, significa que se ha terminado de leer la serie
-  if (n == 0) {
-    return 0;
-  } else {
-    // Suma el número actual con la suma de los números anteriores
-    return n + suma(n - 1);
-  }
-}
-
-int main() {
-  // Declaramos una variable para almacenar la suma
-  int suma = 0;
-
-  // Leemos el primer número
-  int n;
-  scanf("%d", &n);
-
-  // Mientras el número no sea 0, seguimos leyendo
-  while (n != 0) {
-    // Sumamos el número leído a la suma
-    suma += n;
-
-    // Leemos el siguiente número
-    scanf("%d", &n);
-  }
-
-  // Imprimimos la suma
-  printf("La suma es %d\n", suma);
-
-  return 0;
-}
-*/
