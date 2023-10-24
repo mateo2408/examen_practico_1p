55ff9a6b68f4dbc37a017d80a4a09b806c5d6da0
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-24 14:24:24 -0500

    Creacion de programa

---
+++ examen_practico.c
@@ -0,0 +1,4 @@
+/*Utilizando el concepto de función, construya un programa que calcule la 
+suma de una serie de números enteros leídos del teclado. 
+El programa debe detenerse cuando el usuario ingrese el número 0.*/
+//Mateo Cisneros
