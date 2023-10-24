dd70994b14eda2440f3fc1c35661f513cc2624fc
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-10-24 14:31:51 -0500

    Creacion de funcion suma

--- examen_practico.c
+++ examen_practico.c
@@ -2,3 +2,10 @@
 suma de una serie de números enteros leídos del teclado. 
 El programa debe detenerse cuando el usuario ingrese el número 0.*/
 //Mateo Cisneros
+
+#include <stdio.h>
+
+int suma (int sum){
+    int total = sum;
+    return total;
+}
